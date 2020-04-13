7b256a2c9da1c85e2eb9ca952d179cd517bae22a
NathanFS <backentrancehacker@gmail.com>
2020-04-13 07:03:24 +0000

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+
 import './styles/global.css';
 import './styles/App.css';
 import logo from './images/MusicalityLogo.png';
+import Question from './components/Question';
 
-class App extends Component {
+class Home extends Component {
 	render(){
 		return (
-			<main className="fadeIn">
+			<>
+			<main className="home fadeIn">
 				<div className="center-piece">
 					<img id="logo" src={logo} alt="Musicality logo"/>
 					<h1>USICALITY</h1>
@@ -14,10 +23,31 @@ class App extends Component {
 				<p className="sub-title">Generate music because AI can.</p>
 				<div className="center-piece">
 					<button className="classic btn">Get Started</button>
-					
-
 				</div>
 			</main>
+			<Link to="/about">
+				<Question />
+			</Link>
+			</>
+		)
+	}
+}
+
+
+
+class App extends Component {
+	render(){
+		return (
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/about">
+
+					</Route>
+				</Switch>
+			</Router>
 		)
 	}
 }
---
+++ src/components/Question.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+const styles = {
+	position: 'fixed',
+	bottom: '1em',
+	left: '1em',
+	borderRadius: '50%',
+	backgroundColor: 'silver',
+	color: '#fff',
+	padding: '0.5em',
+	height: '2.5em',
+	width: '2.5em',
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	justifyContent: 'center',
+	cursor: 'pointer'
+}
+
+class Question extends Component {
+	render(){
+		return(
+			<div style={styles}>
+				<i className="fas fa-question"></i>
+			</div>
+		)
+	}
+}
+export default Question;
