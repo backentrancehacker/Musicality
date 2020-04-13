962954ed86aee84cce1daa8ab653effb7704d673
NathanFS <backentrancehacker@gmail.com>
2020-04-13 05:27:29 +0000

    Homepage looking hot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import './styles/global.css';
+import './styles/App.css';
+import logo from './images/MusicalityLogo.svg';
 
 class App extends Component {
 	render(){
 		return (
-			<>
-				<p>Hello World</p>
-			</>
+			<main>
+				<div className="center-piece">
+					<img id="logo" src={logo} alt="Musicality logo"/>
+					<h1>USICALITY</h1>
+				</div>
+				<p className="sub-title">Generate music because AI can.</p>
+				<div className="center-piece">
+					<button className="btn">Get Started</button>
+				</div>
+			</main>
 		)
 	}
 }
