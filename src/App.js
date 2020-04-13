9bde5d3d3b9d0da8977abcc3a2806330d7bc9c9c
NathanFS <backentrancehacker@gmail.com>
2020-04-13 06:05:38 +0000

    Mor animation on Home page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import './styles/global.css';
 import './styles/App.css';
-import logo from './images/MusicalityLogo.svg';
+import logo from './images/MusicalityLogo.png';
 
 class App extends Component {
 	render(){
 		return (
-			<main>
+			<main className="fadeIn">
 				<div className="center-piece">
 					<img id="logo" src={logo} alt="Musicality logo"/>
 					<h1>USICALITY</h1>
 				</div>
 				<p className="sub-title">Generate music because AI can.</p>
 				<div className="center-piece">
-					<button className="btn">Get Started</button>
+					<button className="classic btn">Get Started</button>
+					
+
 				</div>
 			</main>
 		)
